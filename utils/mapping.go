dc43ccb946b6ca4881f52b164a6b399a8df13d28
rezafiansyah <rezafiansyah.putra@gmail.com>
2023-09-12 12:16:29 +0700

    finalizing send email to real subject

--- utils/mapping.go
+++ utils/mapping.go
@@ -2,12 +2,9 @@ package utils
 
 func EmailSubjectMapping(subject string) (email string) {
 	subjectToEmailMap := map[string]string{
-		// "Partnership":              "benyaminwidjojo@bwintl.com",
-		// "Be Our Official Retailer": "sales@bwintl.com",
-		// "Customer Service":         "customerservice@bwintl.com",
-		"Partnership":              "monicapangestu23@gmail.com",
-		"Be Our Official Retailer": "monicapangestudsgn@gmail.com",
-		"Customer Service":         "monicapangestu23@gmail.com",
+		"Partnership":              "benyaminwidjojo@bwintl.com",
+		"Be Our Official Retailer": "sales@bwintl.com",
+		"Customer Service":         "customerservice@bwintl.com",
 	}
 
 	return subjectToEmailMap[subject]
